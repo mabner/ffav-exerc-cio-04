7ec37755d963dcf2a0dbfa5868d70612db8b7f4a
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-05-27 02:00:34 -0300

    Searchbar test

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,22 @@
+import { EventEmitter } from '@angular/core';
+import { Output } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  filter = '';
+  newFilterEvent = new EventEmitter<string>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onInputChange(name: string) {
+    this.newFilterEvent.emit(name);
+    console.log(name);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
