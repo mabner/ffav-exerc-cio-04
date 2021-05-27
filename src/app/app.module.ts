2fc587c5fa447d28f302638ecae12221f547802c
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-05-27 04:50:37 -0300

    Search and listing working
    Had to remove some options from the tsconfig.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 import { MovieComponent } from './components/movie/movie.component';
 import { SearchComponent } from './components/search/search.component';
-import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,6 +1,4 @@
-import { EventEmitter } from '@angular/core';
-import { Output } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -8,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-  filter = '';
-  newFilterEvent = new EventEmitter<string>();
+  @Output() filter = '';
+  @Output() newFilterEvent = new EventEmitter<string>();
 
   onInputChange(name: string) {
     this.newFilterEvent.emit(name);
