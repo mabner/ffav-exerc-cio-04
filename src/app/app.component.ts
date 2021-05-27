42d45a2021d2b3628645c104602cebb566861199
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-05-27 01:53:42 -0300

    Implements movies list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'exercicio04';
+  title = 'Exercicio 04 - Movies App';
 }
